b30bbff02bfc1cb8be03f2ad759dd5dbd2dbf255
Gabriel Silva <gabrielgbr.contato@hotmail.com>
2024-03-27 18:30:11 -0300

    Alterações nas entidades

--- src/Domain/Entities/EntityBase.cs
+++ src/Domain/Entities/EntityBase.cs
@@ -3,4 +3,9 @@ public abstract class EntityBase
 {
     public Guid Id { get; set; }
     public DateTime DataCadastro { get; set; }
+    protected EntityBase()
+    {
+        Id = Guid.NewGuid();
+        DataCadastro = DateTime.Now;
+    }
 }
